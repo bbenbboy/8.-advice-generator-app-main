bc77fdc1145dabad9f0ac9dcced51b9593621a0b
Ratchapon Ngamdan <ratchapon.ngam@gmail.com>
2023-02-01 08:22:03 +0700

    Fixed button to generate 1 advice

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const adviceContents = document.querySelector(".advice");
 const container = document.querySelector(".container");
 const btn = document.querySelector(".circle");
 
-const renderAdvice = function (adviceData) {
-  const html = `<div class="card-advice">
-    <p>Advice #<span class="advice-num">${adviceData.id}</span></p>
-    <h2>"<span class="advice">${adviceData.advice}</span>"</h2>
-  </div> `;
-  container.insertAdjacentHTML("beforebegin", html);
-};
+// const renderAdvice = function (adviceData) {
+//   const html = `<div class="card-advice">
+//     <p>Advice #<span class="advice-num">${adviceData.id}</span></p>
+//     <h2>"<span class="advice">${adviceData.advice}</span>"</h2>
+//   </div> `;
+//   container.insertAdjacentHTML("beforebegin", html);
+// };
 
 const getAdviceData = function () {
   fetch("https://api.adviceslip.com/advice")
@@ -22,7 +22,8 @@ const getAdviceData = function () {
       //   console.log(adviceData);
       const adviceObject = adviceData.slip;
       //   console.log(adviceObject);
-      renderAdvice(adviceObject);
+      adviceNum.textContent = `${adviceObject.id}`;
+      adviceContents.textContent = `"${adviceObject.advice}"`;
     })
     .catch((err) => {
       console.log(err);
@@ -30,6 +31,9 @@ const getAdviceData = function () {
 };
 getAdviceData();
 
-btn.addEventListener("click", () => {
-  getAdviceData();
-});
+function click() {
+  btn.addEventListener("click", () => {
+    getAdviceData();
+  });
+}
+click();
